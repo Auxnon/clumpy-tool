7c6645d5b0252a7e0bd905f6a3661feb9556a3b5
Makeavoy <auxnon@gmail.com>
2021-06-03 02:10:54 -0400

    feat: refined build process,scss loads properly, element interface "bubble" likely to be renamed

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 "use strict";
 import "./style/main.scss";
+
+
+class Bubble extends HTMLElement{
+    x:number=0;
+    y:number=0;
+}
+
+let bubbles:Bubble[]=[];
+
 function init():void{
     console.log('hi');
+    document.querySelectorAll('.bubble').forEach(b=>{
+        bubbles.push(b as Bubble);
+    });
+    initListeners();
 }init();
 
 
-function initListens(){
+function initListeners(){
     window.addEventListener('pointerdown',pointerdown);
     window.addEventListener('pointermove',pointermove);
     window.addEventListener('pointerup',pointerup)
@@ -14,6 +27,7 @@ function initListens(){
 
 function pointerdown(ev:PointerEvent){
     console.log(ev.clientX);
+    pos(bubbles[0],ev.clientX,ev.clientY);
 }
 
 function pointermove(ev:PointerEvent){
@@ -21,4 +35,11 @@ function pointermove(ev:PointerEvent){
 }
 function pointerup(ev:PointerEvent){
 
+}
+
+function pos(element:Bubble,x:number,y:number):void{
+    element.x=x;
+    element.y=y;
+    element.style.left=x+"px";
+    element.style.top=y+"px";
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,21 +13,23 @@ module.exports = {
             verbose: true
         }),
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            inject: 'body',
+            template: './src/index.html',
+            filename: 'index.html'
         }),
     ],
     module: {
         rules: [{
-            test: /\.tsx?$/,
-            use: ['ts-loader'],
-            exclude: /node_modules/,
-        }, 
-        {
-            test: /\.s[ac]ss$/i,
-            use: ['css-loader','sass-loader'],
-            exclude: /node_modules/,
-        },
-    ],
+                test: /\.tsx?$/,
+                use: ['ts-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader','css-loader', 'sass-loader'],
+                exclude: /node_modules/,
+            },
+        ],
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
